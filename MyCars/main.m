c0837d7b93fe6f55e5a0acad0d38934c2de706c2
Vasiliy <bazil.06@list.ru>
2013-09-25 13:05:51 +0300

    Initial Commit

---
+++ MyCars/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  MyCars
+//
+//  Created by Vasiliy on 25.09.13.
+//  Copyright (c) 2013 Vasiliy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
